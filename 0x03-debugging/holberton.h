32e496bbda7b8817634804b549a43f99cb466817
Trice254 <tricemegamind254@gmail.com>
2021-06-24 10:32:50 -0700

    holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 
-char _putchar(char c);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
