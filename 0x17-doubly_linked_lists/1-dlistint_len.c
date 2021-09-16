cd2782b117763aa1703bf587994f46d66c85af97
Trice254 <tricemegamind254@gmail.com>
2021-09-16 10:47:43 -0700

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,38 +1,19 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * add_dnodeint - add node to front of doubly linked list
- * @head: pointer to head of list
- * @n: node data
- * Return: address of new node, or Null if failed
+ * dlistint_len - counts and returns num elements in doubly linked list
+ * @h: pointer to head of list
+ * Return: number of elements
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+size_t dlistint_len(const dlistint_t *h)
 {
-	dlistint_t *new;
+	size_t nodes = 0;
 
-	if (head == NULL)
-		return (NULL);
-
-	/* malloc and set new node data */
-	new = malloc(sizeof(struct dlistint_s));
-	if (!new)
-		return (NULL);
-	new->n = n;
-
-	/* account for empty linked list */
-	if (*head == NULL)
+	while (h)
 	{
-		*head = new;
-		new->next = NULL;
-		new->prev = NULL;
-		return (new);
+		h = h->next;
+		nodes += 1;
 	}
 
-	/* account for regular linked list */
-	new->next = *head;
-	new->prev = NULL;
-	(*head)->prev = new;
-	*head = new;
-	return (new);
+	return (nodes);
 }
